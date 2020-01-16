8815302bce5b3a85d8f404691b58433cc7ccd5aa
nawazsk <nawaz.540@gmail.com>
2020-01-17 02:36:30 +0530

    Updates

---
+++ precache-manifest.29f3a2811f040a533917df14c0dbd818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5126a3498c137ce8f71984055a76c4dd",
+    "url": "/robot-vaccum/index.html"
+  },
+  {
+    "revision": "01fb664077f5c6167d7c",
+    "url": "/robot-vaccum/static/css/main.db57c9fa.chunk.css"
+  },
+  {
+    "revision": "818c7117320c7357a823",
+    "url": "/robot-vaccum/static/js/2.f2989819.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robot-vaccum/static/js/2.f2989819.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01fb664077f5c6167d7c",
+    "url": "/robot-vaccum/static/js/main.d0528a9f.chunk.js"
+  },
+  {
+    "revision": "f11a5e867b505e37c010",
+    "url": "/robot-vaccum/static/js/runtime-main.90289187.js"
+  }
+]);
